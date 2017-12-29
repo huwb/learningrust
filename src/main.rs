b3dc17d68a2dd7d932ea9f2dbefcae70abfb9e04
Huw Bowles <huw.bowles@gmail.com>
2017-12-29 23:39:42 +0000

    Current state of main

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,28 @@
-extern crate rand;
+use std::thread;
 
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+fn run() -> () {
+    let handles: Vec<_> = (0..10)
+        .map(|_| {
+            thread::spawn(|| {
+                let mut x = 0;
+                for _ in 0..5_000_000 {
+                    x += 1;
+                }
+                x
+            })
+        })
+        .collect();
 
-fn main()
-{
-    println!( "Guess the number!" );
-    println!( "-===============-" );
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    let mut guess_count = 1;
-    
-    loop
-    {
-        println!("Input your guess [{}]", guess_count);
-
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(error) => {
-                println!("Invalid input: {}", error );
-                continue;
-            }
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less      => println!("Too small!"),
-            Ordering::Greater   => println!("Too big!"),
-            Ordering::Equal     => {
-                println!("You win! Attempts: {}", guess_count);
-                break;
-            }
-        }
-
-        guess_count += 1;
+    for h in handles {
+        println!(
+            "Thread finished with count = {}",
+            h.join().map_err(|_| "Could not join a thread!").unwrap()
+        );
     }
+}
+
+fn main() {
+    run();
+
+    println!("Done!");
 }
