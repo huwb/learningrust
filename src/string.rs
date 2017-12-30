fb3e1260d7890307b9bd59376c05660a341a79ee
Huw Bowles <huw.bowles@gmail.com>
2017-12-30 11:36:34 +0000

    First word in string

---
+++ src/string.rs
@@ -0,0 +1,19 @@
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn main() {
+    let s = String::from("hello");
+
+    let s_f = first_word(&s);
+
+    println!("{} first in {}", s_f, s);
+}
