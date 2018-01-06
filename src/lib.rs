e52800626f50a49ac135958283b8b53ee7e7e81e
Huw Bowles <huw.bowles@gmail.com>
2018-01-06 21:42:39 +0000

    Fit standard format for binaries - trivial main.rs

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+pub mod generic_fn;
+
+pub fn longest_string<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+#[test]
+fn test_longest_string() {
+    let x = "short";
+    let y = "long";
+    let longest = longest_string(x, y);
+    assert_eq!(longest, x);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-pub mod guessing_game;
+extern crate learningrust;
 
 fn main() {
-    guessing_game::run_game();
 
-    println!("hello");
+    let hello;
+    let longest;
+    {
+        hello = "hello";
+        let goodbye = "goodbye";
+        longest = learningrust::longest_string(hello, goodbye);
+    }
+
+    println!("Longest: {}", longest);
+
+    let list = [4, 5, 3, 1, 2];
+    let largest = learningrust::generic_fn::_largest_in_list(&list);
+    println!("Largest: {}", largest);
 }
