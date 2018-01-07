3e240dc15668146d8ce0b129fe718d09842f1d3a
Huw Bowles <huw.bowles@gmail.com>
2018-01-07 17:46:01 +0000

    Added counter iterator + tests

--- src/iterators.rs
+++ src/iterators.rs
@@ -1,6 +1,50 @@
+pub struct Counter {
+    counter: u32,
+}
+
+impl Counter {
+    pub fn new() -> Counter {
+        Counter { counter: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.counter += 1;
+        match self.counter {
+            i if i < 6 => Some(i),
+            _ => None,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
+
+    #[test]
+    fn counter_calling_next() {
+        let mut c = Counter::new();
+        assert_eq!(c.next(), Some(1));
+        assert_eq!(c.next(), Some(2));
+        assert_eq!(c.next(), Some(3));
+        assert_eq!(c.next(), Some(4));
+        assert_eq!(c.next(), Some(5));
+        assert_eq!(c.next(), None);
+    }
+
+    #[test]
+    fn counter_other_iterator_traits() {
+        let result: u32 = Counter::new()
+            .zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|a| a % 3 == 0)
+            .sum();
+
+        assert_eq!(result, 18);
+    }
 
     #[test]
     fn iterator_sum() {
