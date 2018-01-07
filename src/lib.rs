1e57277394e6c242d45b56bba07e8ff989455bc4
Huw Bowles <huw.bowles@gmail.com>
2018-01-07 17:49:55 +0000

    Moved main to run function to match standards

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,26 @@ pub mod generic_fn;
 pub mod closures;
 pub mod iterators;
 
+pub fn run() {
+    let hello;
+    let longest;
+    {
+        hello = "hello";
+        let goodbye = "goodbye";
+        longest = longest_string(hello, goodbye);
+    }
+
+    println!("Longest: {}", longest);
+
+    let list = [4, 5, 3, 1, 2];
+    let largest = generic_fn::_largest_in_list(&list);
+    println!("Largest: {}", largest);
+
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+    closures::generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
 pub fn longest_string<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,5 @@
 extern crate learningrust;
 
 fn main() {
-    let hello;
-    let longest;
-    {
-        hello = "hello";
-        let goodbye = "goodbye";
-        longest = learningrust::longest_string(hello, goodbye);
-    }
-
-    println!("Longest: {}", longest);
-
-    let list = [4, 5, 3, 1, 2];
-    let largest = learningrust::generic_fn::_largest_in_list(&list);
-    println!("Largest: {}", largest);
-
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
-    learningrust::closures::generate_workout(
-        simulated_user_specified_value,
-        simulated_random_number,
-    );
+    learningrust::run();
 }
