45edfff7260612cfce30802472d7de86e63923b7
Huw Bowles <huw.bowles@gmail.com>
2018-01-08 20:54:44 +0000

    Cons list push and add

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -5,8 +5,28 @@ enum MyList {
 }
 
 impl MyList {
+    fn new() -> MyList {
+        MyList::Nil
+    }
+
     fn cons(value: i32, next: MyList) -> MyList {
         MyList::Cons(value, Box::new(next))
+    }
+
+    /// Add value to end of list
+    fn add(list: MyList, value: i32) -> MyList {
+        match list {
+            MyList::Cons(val, boxed) => MyList::Cons(val, Box::new(MyList::add(*boxed, value))),
+            MyList::Nil => MyList::Cons(value, Box::new(MyList::Nil)),
+        }
+    }
+
+    /// Push value onto end of list
+    fn push(&mut self, value: i32) {
+        match *self {
+            MyList::Cons(_, ref mut boxed) => boxed.push(value),
+            MyList::Nil => *self = MyList::Cons(value, Box::new(MyList::Nil)),
+        };
     }
 }
 
@@ -16,10 +36,11 @@ pub fn run() {
     let b = Box::new(5);
     println!("{}", b);
 
-    let l = MyList::cons(7, MyList::cons(3, MyList::cons(5, MyList::Nil)));
+    let mut l = MyList::new();
+    l = MyList::cons(29, l);
+    l = MyList::add(l, 33);
+    l.push(11);
     println!("{:?}", l);
-
-    assert_eq!(MyList::cons(7, MyList::Nil), MyList::cons(7, MyList::Nil));
 }
 
 #[cfg(test)]
@@ -36,6 +57,42 @@ mod tests {
                 Box::new(MyList::Cons(
                     3,
                     Box::new(MyList::Cons(5, Box::new(MyList::Nil)))
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn mylist_add() {
+        let mut l = MyList::new();
+        l = MyList::add(l, 3);
+        l = MyList::add(l, 7);
+        l = MyList::add(l, 5);
+        assert_eq!(
+            l,
+            MyList::Cons(
+                3,
+                Box::new(MyList::Cons(
+                    7,
+                    Box::new(MyList::Cons(5, Box::new(MyList::Nil)))
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn mylist_push() {
+        let mut l = MyList::new();
+        l.push(9);
+        l.push(6);
+        l.push(8);
+        assert_eq!(
+            l,
+            MyList::Cons(
+                9,
+                Box::new(MyList::Cons(
+                    6,
+                    Box::new(MyList::Cons(8, Box::new(MyList::Nil)))
                 ))
             )
         );
