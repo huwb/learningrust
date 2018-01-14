1bf3becb0ffb0e2b0bd730d749a9a9736deaccc9
Huw Bowles <huw.bowles@gmail.com>
2018-01-14 00:01:15 +0000

    Using iterator on channel, much nicer

--- src/threads_intro.rs
+++ src/threads_intro.rs
@@ -7,25 +7,16 @@ pub fn run() {
 
     thread::spawn(move || {
         println!("[SPAWNED] Typing message from spawned thread...");
-        thread::sleep(time::Duration::from_millis(5000));
+        thread::sleep(time::Duration::from_millis(3000));
         let val = String::from("Hellooooo from the other thread..");
         println!("[SPAWNED] Sending message!");
         tx.send(val).unwrap();
+        println!("[SPAWNED] Goodbye!");
     });
 
-    let mut got_it = false;
-
-    while !got_it {
-        match rx.try_recv() {
-            Ok(result) => {
-                println!("[MAIN] Message received!");
-                println!("[MAIN] Message: {:?}", result);
-                got_it = true;
-            }
-            Err(details) => {
-                println!("[MAIN] Got an error: {}", details);
-                thread::sleep(time::Duration::from_millis(1000));
-            }
-        };
+    for received in rx {
+        println!("[MAIN] Message received: {:?}", received);
     }
+
+    println!("[MAIN] Channel terminated");
 }
