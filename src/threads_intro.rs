2d36a6d6a3bf0249deef9f36314df38da5c47775
Huw Bowles <huw.bowles@gmail.com>
2018-01-14 00:14:43 +0000

    Receive messages from multiple senders

--- src/threads_intro.rs
+++ src/threads_intro.rs
@@ -5,14 +5,20 @@ use std::time;
 pub fn run() {
     let (tx, rx) = mpsc::channel();
 
-    thread::spawn(move || {
-        println!("[SPAWNED] Typing message from spawned thread...");
-        thread::sleep(time::Duration::from_millis(3000));
-        let val = String::from("Hellooooo from the other thread..");
-        println!("[SPAWNED] Sending message!");
-        tx.send(val).unwrap();
-        println!("[SPAWNED] Goodbye!");
-    });
+    for i in 1..10 {
+        let txi = mpsc::Sender::clone(&tx);
+        thread::spawn(move || {
+            println!("[SPAWNED{}] Typing message from spawned thread...", i);
+            thread::sleep(time::Duration::from_millis(3000));
+            let val = format!("Hellooooo from thread {}", i);
+            println!("[SPAWNED{}] Sending message!", i);
+            txi.send(val).unwrap();
+            println!("[SPAWNED{}] Goodbye!", i);
+        });
+    }
+    // needed to close all channels. not very nice but I wasn't able to nicely clone
+    // for 9 threads after trying a couple of approaches.
+    drop(tx);
 
     for received in rx {
         println!("[MAIN] Message received: {:?}", received);
